f55d89297230f6bd0021f4c53d5d5c6a5ecd93ea
wch <1165553576@qq.com>
2022-12-12 23:08:16 +0800

    解包前

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+import '../node_modules/antd/dist/antd';	// 引入antd的样式
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
